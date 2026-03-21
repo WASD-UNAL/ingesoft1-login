e2caed9e1e0233ba831437011085b3a3ddb811ca
sebasO1 <solayas@unal.edu.co>
2026-03-21 15:52:14 -0500

    backend implementation

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <LoginForm @login-success="handleLoginSuccess"/>
+    </div>
+</template>
+
+<script>
+    import LoginForm from '../components/LoginForm.vue';
+    
+    export default{
+        name: "LoginView",
+        components: {
+            LoginForm
+        },
+        methods:{
+            handleLoginSuccess(secretPhrase){
+                this.$router.push({
+                    name:"Secret",
+                    params:{
+                        secretPhrase:secretPhrase
+                    }
+                })
+            }
+        }
+    }
+</script>
